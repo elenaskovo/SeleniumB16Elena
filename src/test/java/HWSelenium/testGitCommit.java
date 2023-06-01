c447889590f407a1352dd14ac9c04fcc3bf22d12
elenaskovo <elenaskovorodko@Elenas-Air.lan>
2023-06-01 14:38:53 -0400

    test file

--- src/test/java/HWSelenium/testGitCommit.java
+++ src/test/java/HWSelenium/testGitCommit.java
@@ -2,6 +2,8 @@ package HWSelenium;
 
 public class testGitCommit {
     public static void main(String[] args) {
+
+        System.out.println("Hello");
         System.out.println("Hello");
     }
 }
